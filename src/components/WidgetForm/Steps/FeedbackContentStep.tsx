83d7a9f531d918e8fc18308066e7d3083f88276a
Ariun <thiagotatico007@gmail.com>
2022-05-03 17:05:49 -0300

    Changes

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,7 @@
-import { ArrowArcRight, ArrowLeft } from "phosphor-react";
+import { ArrowArcRight, ArrowLeft, Camera } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
@@ -35,8 +36,17 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested}:
           placeholder="Conte com detalhes o que está acontecendo..."
         />
 
-        <footer>
-          
+        <footer className="flex gap-2 mt-2">
+          <ScreenshotButton />
+
+          <button
+            type='submit'
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            Enviar Feedback
+          </button>
+
+
         </footer>
       </form>
     </>
