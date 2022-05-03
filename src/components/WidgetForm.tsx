de017ca2dc46d6da2a6b365eba637c1cf8d2697b
Ariun <thiagotatico007@gmail.com>
2022-05-03 11:40:36 -0300

    Changes

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,7 +1,7 @@
 import { CloseButton } from "./CloseButton";
 
 import bugImageUrl from '../assets/bug.svg';
-import ideaImageUrl from '../assets idea.svg';
+import ideaImageUrl from '../assets/idea.svg';
 import thoughtImageUrl from '../assets/thought.svg';
 
 
@@ -39,7 +39,19 @@ export function WidgetForm() {
       </header>
 
       <div className="flex py-8 gap-2 w-full">
+        { Object.entries(feedbackTypes).map(([key, value]) => {
 
+          return (
+            <button
+              key={key}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              type="button"
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span> {value.title} </span>
+            </button>
+          );
+        }) }
       </div>
 
       <footer className="text-xs text-neutral-400">
