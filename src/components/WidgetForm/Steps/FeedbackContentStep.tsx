a00fd328e20357f0bc24dc36b30dd64135d6d499
Ariun <thiagotatico007@gmail.com>
2022-05-03 15:59:15 -0300

    Changes

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -31,9 +31,13 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested}:
       
       <form className="my-4 w-full">
         <textarea
-          className=""
+          className="min-width-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo..."
         />
+
+        <footer>
+          
+        </footer>
       </form>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       colors: {
         brand: {
-          500: '#8257e6',
+          500: '#8257e6'
         }
       }
-    },
+    }
   },
-  plugins: [],
-}
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar')
+  ]
+};
